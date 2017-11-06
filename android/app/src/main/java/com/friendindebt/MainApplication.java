4a67bb36a42bb46e59a06a1cf12945045d4b45c0
Matt Smithies <matt.smithies@theassetworks.com>
2017-11-06 16:53:17 +0000

    Running android with react native

--- android/app/src/main/java/com/friendindebt/MainApplication.java
+++ android/app/src/main/java/com/friendindebt/MainApplication.java
@@ -2,13 +2,15 @@ package com.friendindebt;
 
 import android.app.Application;
 
-import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.bitgo.randombytes.RandomBytesPackage;
+import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.oblador.vectoricons.VectorIconsPackage;
+
+import org.pgsqlite.SQLitePluginPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,9 +26,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VectorIconsPackage(),
-            new RandomBytesPackage()
+              new SQLitePluginPackage(),
+              new VectorIconsPackage(),
+              new RandomBytesPackage(),
+              new MainReactPackage()
       );
     }
 
