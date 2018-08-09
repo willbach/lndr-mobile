d66b138277254eca6f29ceb71bab4f8361110638
Will Galebach <willbach@gmail.com>
2018-08-09 12:26:51 -0500

    First round of last-minute fixes for 1.2.0

--- packages/ui/views/account/activity/pending/index.tsx
+++ packages/ui/views/account/activity/pending/index.tsx
@@ -45,6 +45,11 @@ interface Props {
   getPending: () => any
 }
 
+interface PassedProps extends React.Props<any> {
+  navigation: any
+  homeScreen?: boolean
+}
+
 interface State {
   pendingTransaction?: PendingTransaction
 }
@@ -220,6 +225,6 @@ class PendingView extends Component<Props, State> {
   }
 }
 
-export default connect((state) => ({ state: getStore(state)(), user: getUser(state)(), isFocused: isFocusingOn(state)('Activity'),
+export default connect<any, any, PassedProps>((state) => ({ state: getStore(state)(), user: getUser(state)(), isFocused: isFocusingOn(state)('Activity'),
 pendingSettlements: pendingSettlements(state), bilateralSettlements: bilateralSettlements(state), submitterIsMe: submitterIsMe(state),
 settlerIsMe: settlerIsMe(state), payPalRequests: payPalRequests(state) }), { getPending })(PendingView)
